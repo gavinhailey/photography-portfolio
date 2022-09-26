4ccd13ae2dacb7bd95b9d244e40f8bf1d85df93e
Gavin Hailey <gavin.g.hailey@gmail.com>
2022-09-25 19:34:21 -0600

    portfolio update and masonry

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,20 +7,37 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-emilia-core/gatsby-config.js
-    siteTitle: `Emilia`,
-    siteTitleAlt: `Emilia - Gatsby Starter Portfolio`,
-    siteHeadline: `Emilia - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://emilia.lekoarts.de`,
-    siteDescription: `Minimalistic portfolio/photography site with masonry grid, page transitions and big images. Themeable with Theme UI. Includes Light/Dark mode.`,
+    siteTitle: `Gavin Hailey's Photography`,
+    siteTitleAlt: `Gavin Hailey's Photography Portfolio`,
+    siteHeadline: `Gavin Hailey's Photography`,
+    siteUrl: `https://photo.gavinhailey.dev`,
+    siteDescription: `Collection of my hobbiest photography over the last few years`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@gavinhailey`,
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-emilia`,
       // See the theme's README for all available options
-      options: {},
+      options: {
+        name: `Gavin Hailey`,
+        location: `Denver, CO`,
+        socialMedia: [{
+          href: `https://github.com/gavinhailey`,
+          title: `GitHub`
+        },
+        {
+          href: `https://www.instagram.com/gavinhailey/`,
+          title: `Instagram`
+        },
+        { 
+          href: `https://www.linkedin.com/in/gavin-hailey/`,
+          title: `LinkedIn`
+        }],
+        showThemeAuthor: false,
+        formatString: `MM/DD/YYYY`,
+      },
     },
     {
       resolve: `gatsby-plugin-sitemap`,
